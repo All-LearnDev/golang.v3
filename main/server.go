bac1e9558c6b733f8a92cdabd4192374d8da802d
Dinh Nguyen <dinhnguyen.techs@gmail.com>
2022-03-31 16:11:57 +0700

    Add cors to middleware

--- main/server.go
+++ main/server.go
@@ -5,12 +5,14 @@ import (
 	"projects/routers"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
 
 	configs.AutoMigrate()
 	rest := echo.New()
+	rest.Use(middleware.CORS())
 	routers.InitializeApiMapping(rest)
 	rest.Logger.Fatal(rest.Start(":1323"))
 
